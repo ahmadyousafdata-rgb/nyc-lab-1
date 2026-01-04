a8b37686343fbc83b0816ff3dc63f7a8a6fea224
ahmadyousafdata-rgb <ahmadyousafdata@gmail.com>
2026-01-04 18:42:48 -0500

    Switch Calendly inline embed to script and drop react-calendly

--- src/components/CalendlyInline.jsx
+++ src/components/CalendlyInline.jsx
@@ -1,13 +1,22 @@
 'use client'
 
-import { InlineWidget } from 'react-calendly'
+import Script from 'next/script'
 
 export default function CalendlyInline() {
   return (
     <div className="w-full overflow-hidden rounded-xl border border-slate-200 bg-white shadow-sm">
-      <InlineWidget
-        url="https://calendly.com/founder-nyxlab/30min"
-        styles={{ minWidth: '320px', height: '700px' }}
+      <link
+        href="https://assets.calendly.com/assets/external/widget.css"
+        rel="stylesheet"
+      />
+      <Script
+        src="https://assets.calendly.com/assets/external/widget.js"
+        strategy="lazyOnload"
+      />
+      <div
+        className="calendly-inline-widget"
+        data-url="https://calendly.com/founder-nyxlab/30min"
+        style={{ minWidth: '320px', height: '700px' }}
       />
     </div>
   )
